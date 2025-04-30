823b27089d177c9b46893868da61de51aeeae138
itznuth <anthonynuth70@gmail.com>
2025-04-29 18:04:27 -0700

    Update financialreporting.py

--- Code/financialreporting.py
+++ Code/financialreporting.py
@@ -1,16 +1,32 @@
-class FinancialReporting:
-    """Represents financial reporting for the mall."""
+class Investor:
+    """Represents an investor in the mall.
 
-    def __init__(self, total_revenue: float, total_sales: float, sales_date: str):
-        self.total_revenue = total_revenue  
-        self.total_sales = total_sales      
-        self.sales_date = sales_date        
+    Attributes:
+        name (str): The name of the investor.
+        contact_information (str): Contact details for the investor.
+    """
 
-    def get_sales_date(self) -> str:
-        return self.sales_date
+    def __init__(self, name: str, contact_information: str):
+        """Initializes an Investor object with the given name and contact information."""
+        self.name = name
+        self.contact_information = contact_information
 
-    def get_revenue(self) -> float:
-        return self.total_revenue
+    def set_name(self, name: str) -> None:
+        """Sets the name of the Investor."""
+        self.name = name
 
-    def get_sales(self) -> float:
-        return self.total_sales
+    def set_contact_information(self, contact_information: str) -> None:
+        """Sets the contact information of the Investor."""
+        self.contact_information = contact_information
+
+    def review_financial_reports(self) -> str:
+        """Creates a statement keeping track of who is reviewing financial reports."""
+        return f"{self.name} is reviewing the financial reports"
+
+    def get_contact_information(self) -> str:
+        """Returns the contact information of the Investor."""
+        return self.contact_information
+
+    def get_name(self) -> str:
+        """Returns the name of the Investor."""
+        return self.name
